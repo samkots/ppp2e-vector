1ab2879a8530523e3b51a4a3b40912f719f3dbc4
Sameer Kotasthane <samkots@gmail.com>
2017-11-11 16:59:55 +0530

    test case for subscript added

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() try
 {
 	using vst = vector::size_type;
 
-	{ BLOCK("ctor")
+	{ BLOCK("ctor v, vv(4)")
 		vector v;
 		cout << "v.szie: " << v.size() << endl;
 		cout << "v.capacity: " << v.capacity() << endl;
@@ -22,6 +22,15 @@ int main() try
 		cout << "vv.capacity: " << vv.capacity() << endl;
 
 		for (vst i = 0; i < vv.size(); ++i) cout << vv[i] << '\n';
+	}
+
+	{ BLOCK("subscript v(4)")
+		vector v(4);
+		cout << "v[2]: " << v[2] << endl;
+		cout << "v.at(2): " << v.at(2) << endl;
+
+		//cout << "v[4]: " << v[4] << endl;
+		//cout << "v.at(4): " << v.at(4) << endl;
 	}
 }
 catch (const bad_alloc& x) {
